e6bfc8b2986fcae74c0f38db91522868de776ea1
olsemeniuk <ol.semeniuk.mail@gmail.com>
2024-04-21 19:48:56 +0300

    removed ibl and rainbow plugins

--- lua/plugins/setup.lua
+++ lua/plugins/setup.lua
@@ -41,8 +41,6 @@ return {
 		"goolord/alpha-nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-	{ "HiPhish/rainbow-delimiters.nvim" },
 	{ "williamboman/mason.nvim", lazy = false },
 	{ "williamboman/mason-lspconfig.nvim", lazy = false },
 	{ "neovim/nvim-lspconfig", lazy = false },
